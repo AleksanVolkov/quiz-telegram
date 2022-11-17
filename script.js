bc36506cbdbf62c4cbf54824722fd8d834933cbd
Alexandr <kodak333vas@gmail.com>
2022-11-17 18:02:05 +0300

    2

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const form = document.querySelector('#form').addEventListener('submit', function(e){
+    e.preventDefault();
+
+
+   const errorM = document.querySelector('#error');
+    
+
+    let TOKEN = '5791633342:AAEOD_eKDntfzgcOco4R0WbOB9jN-SMXGyY';
+    let URI_API =`https://api.telegram.org/bot${ TOKEN }/sendMessage`;
+    let CHAT_Id = '-1001656232893';
+    
+    
+    let message = '<b>эзаявка из сайта</b>\n';
+
+    message+=`<b>пользватель:</b>${this.name.value}\n`;
+    message+=`<b>Email:</b>${this.email.value}`;
+
+    console.log(message)
+
+
+
+    axios.post(URI_API,{
+        chat_id:CHAT_Id,
+        parse_mode:'html',
+        text:message
+    }).then((res)=>{
+        this.name.value='';
+        this.email.value='';
+        const alert = document.querySelector('#message');
+        alert.style.display="block";
+        setTimeout(()=>{alert.style.display="none";},'1000')
+    }).catch((error)=>{
+        errorM.style.display="block";
+        errorM.textContent =`собщение не отправленно!!!  код ошибки: ${error}`;
+    })
+
+
+
+
+});
